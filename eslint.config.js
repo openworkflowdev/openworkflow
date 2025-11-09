37bb527f35b98450c2230cf36acdf95b92fb4cd3
James Martinez <jamescmartinez@gmail.com>
2025-11-09 13:08:58 -0600

    Switch from eslint project: true to projectService: true

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ export default defineConfig(
   {
     languageOptions: {
       parserOptions: {
-        project: true,
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
