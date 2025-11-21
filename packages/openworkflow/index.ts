fd5f8bf4fc012e054defc1b7185f4d32e0ee9d6f
James Martinez <jamescmartinez@gmail.com>
2025-11-21 12:24:44 -0800

    Users don't need schema types

--- packages/openworkflow/index.ts
+++ packages/openworkflow/index.ts
@@ -2,7 +2,6 @@ export type { OpenWorkflowOptions } from "./client.js";
 export { OpenWorkflow } from "./client.js";
 
 export * from "./backend.js";
-export * from "./schema.js";
 
 export type { WorkerOptions } from "./worker.js";
 export { Worker } from "./worker.js";
