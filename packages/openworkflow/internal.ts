7aecd6caf3788692b597f5c6a89aa3dd604b2fa9
James Martinez <jamescmartinez@gmail.com>
2026-02-02 16:35:18 -0600

    Add back several core packages for dashboard

--- packages/openworkflow/internal.ts
+++ packages/openworkflow/internal.ts
@@ -5,3 +5,7 @@ export { isWorkflow } from "./workflow.js";
 // backend
 export type { Backend } from "./backend.js";
 export { testBackend } from "./backend.testsuite.js";
+
+// core
+export type { WorkflowRun, WorkflowRunStatus } from "./core/workflow.js";
+export type { StepAttempt, StepAttemptStatus, StepKind } from "./core/step.js";
