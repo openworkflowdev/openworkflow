20eba7176d2a6cdb23019a2571a5943fbdf26f25
James Martinez <jamescmartinez@gmail.com>
2026-02-02 13:48:08 -0600

    Clean up internal exports

--- packages/openworkflow/internal.ts
+++ packages/openworkflow/internal.ts
@@ -3,11 +3,5 @@ export type { Workflow } from "./workflow.js";
 export { isWorkflow } from "./workflow.js";
 
 // backend
-export * from "./backend.js";
+export type { Backend } from "./backend.js";
 export { testBackend } from "./backend.testsuite.js";
-
-// core
-export type { JsonValue } from "./core/json.js";
-export type { WorkflowRun, WorkflowRunStatus } from "./core/workflow.js";
-export type { StepAttempt, StepAttemptStatus, StepKind } from "./core/step.js";
-export { DEFAULT_RETRY_POLICY } from "./core/retry.js";
