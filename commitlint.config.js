cf9397e805d9d242784c0f190b39a1d34234af6f
James Martinez <jamescmartinez@gmail.com>
2026-02-23 10:25:45 -0600

    chore: commitlint enforce workspace scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,29 @@
-export default { extends: ["@commitlint/config-conventional"] };
+import scopesConfig from "@commitlint/config-workspace-scopes";
+
+export default {
+  extends: [
+    "@commitlint/config-conventional",
+    "@commitlint/config-workspace-scopes",
+  ],
+  rules: {
+    "scope-enum": async (ctx) => {
+      const [severity, when, scopes] =
+        await scopesConfig.rules["scope-enum"](ctx);
+      return [
+        severity,
+        when,
+        [
+          ...scopes,
+          "deps", // Dependabot
+          "dev-deps", // Dependabot
+          "release", // release commits
+        ],
+      ];
+    },
+  },
+  prompt: {
+    settings: {
+      enableMultipleScopes: true,
+    },
+  },
+};
